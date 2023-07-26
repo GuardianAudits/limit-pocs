6b78efebec85e4ba15a55f4973deaee206bdfca1
Owen Thurm <owenthurm@Owens-Air-2.lan>
2023-07-26 18:10:50 -0400

    PoC8

---
+++ test/guardian/PoC8.ts
@@ -0,0 +1,93 @@
+/* global describe it before ethers */
+const hardhat = require('hardhat')
+const { expect } = require('chai')
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { BigNumber } from 'ethers'
+import { mintSigners20 } from '../utils/token'
+import {
+    BN_ZERO,
+    PoolState,
+    getLiquidity,
+    getPositionLiquidity,
+    getPrice,
+    getSwapEpoch,
+    getTick,
+    getTickAtPrice,
+    validateBurn,
+    validateMint,
+    validateSwap
+} from '../utils/contracts/limitpool'
+import { gBefore } from '../utils/hooks.test'
+
+alice: SignerWithAddress
+describe('LimitPool Tests', function () {
+    let tokenAmount: string
+    let tokenAmountBn: BigNumber
+    let token0Decimals: number
+    let token1Decimals: number
+    let minPrice: BigNumber
+    let maxPrice: BigNumber
+
+    let alice: SignerWithAddress
+    let bob: SignerWithAddress
+    let carol: SignerWithAddress
+
+    const liquidityAmount = BigNumber.from('20051041647900280328782')
+    const minTickIdx = BigNumber.from('-887272')
+    const maxTickIdx = BigNumber.from('887272')
+
+    ////////// DEBUG FLAGS //////////
+    let debugMode           = false
+    let balanceCheck        = false
+    let deltaMaxBeforeCheck = false
+    let deltaMaxAfterCheck  = false
+    let latestTickCheck     = false
+
+    //every test should clear out all liquidity
+
+    before(async function () {
+        await gBefore()
+        let currentBlock = await ethers.provider.getBlockNumber()
+        const pool0: PoolState = await hre.props.limitPool.pool0()
+        const liquidity = pool0.liquidity
+        const globalState = await hre.props.limitPool.globalState()
+        const price = pool0.price
+
+        expect(liquidity).to.be.equal(BN_ZERO)
+
+        minPrice = BigNumber.from('4297706460')
+        maxPrice = BigNumber.from('1460570142285104104286607650833256105367815198570')
+        token0Decimals = await hre.props.token0.decimals()
+        token1Decimals = await hre.props.token1.decimals()
+        tokenAmountBn = ethers.utils.parseUnits('100', token0Decimals)
+        tokenAmount = ethers.utils.parseUnits('100', token0Decimals).toString()
+        alice = hre.props.alice
+        bob = hre.props.bob
+        carol = hre.props.carol
+    })
+
+    this.beforeEach(async function () {
+        await mintSigners20(hre.props.token0, tokenAmountBn.mul(10), [hre.props.alice, hre.props.bob])
+
+        await mintSigners20(hre.props.token1, tokenAmountBn.mul(10), [hre.props.alice, hre.props.bob])
+    })
+
+    it.only('mints will fail when min is beyond certain point  ', async function () {
+
+        // mint reverts as the price becomes out of bounds
+        await validateMint({
+            signer: hre.props.alice,
+            recipient: hre.props.alice.address,
+            lower: '-665460',
+            upper: '-665450',
+            amount: tokenAmount,
+            zeroForOne: true,
+            balanceInDecrease: '100000000000000000000',
+            liquidityIncrease: '804',
+            upperTickCleared: false,
+            lowerTickCleared: true,
+            revertMessage: 'PriceOutOfBounds()',
+        })
+
+    })
+})
